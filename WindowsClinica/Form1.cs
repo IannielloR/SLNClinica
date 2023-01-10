9406e9e861b8bca7b06ae9951c67745ef633d833
IannielloR <rianniello@fi.uba.ar>
2023-01-10 15:32:59 -0300

    Agregar archivos de proyecto.

---
+++ WindowsClinica/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WindowsClinica.Models;//agregar namespace
+
+namespace WindowsClinica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrearMedico_Click(object sender, EventArgs e)
+        {
+            //declarar y crear una instancia de medico
+            Medico medico = new Medico();
+
+            //SER Escribir las propiedades
+            medico.Nombre = "Juan";
+            medico.Apellido = "Perez";
+            medico.Matricula = 123456;
+
+            //Get leer las propiedades
+            MessageBox.Show(medico.Nombre + " " + medico.Apellido + " " + medico.Matricula.ToString());
+        }
+    }
+}
---
+++ WindowsClinica/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsClinica.Models
+{
+    internal class Medico
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Matricula { get; set; }
+    }
+}
